dca22e2d402022388ac970facd38a0b67f701fd7
Krastti <salcev77@gmail.com>
2025-12-09 23:34:54 +0300

    Update /Database/models.py update* template_text new* template_name

--- src/Database/models.py
+++ src/Database/models.py
@@ -1,12 +1,14 @@
 # Библиотеки
 import uuid
 from datetime import datetime
+from typing import Optional
 from pydantic import BaseModel
 
 class Template(BaseModel):
     id: str = str(uuid.uuid4()) # UUID
     user_id: int # ID пользователя
-    template_text: str # Текст шаблона
+    template_name: str # Название шаблона
+    template_text: Optional[str] = None # Текст шаблона
     python_code: str # Код на python?
     created_at: datetime = datetime.now() # Время создания
 
